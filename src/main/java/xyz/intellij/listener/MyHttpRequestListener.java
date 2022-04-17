76436a0b05679a80642cb18e50e8b80105bea695
whp98 <whp98@outlook.com>
2022-04-17 18:12:11 +0800

    http request 监听器

--- src/main/java/xyz/intellij/listener/MyHttpRequestListener.java
+++ src/main/java/xyz/intellij/listener/MyHttpRequestListener.java
@@ -2,7 +2,9 @@ package xyz.intellij.listener;
 
 import javax.servlet.ServletRequestEvent;
 import javax.servlet.ServletRequestListener;
-
+/**
+ * 监听器用于监听httprequest对象的创建和销毁事件
+ * */
 public class MyHttpRequestListener implements ServletRequestListener {
     @Override
     public void requestDestroyed(ServletRequestEvent servletRequestEvent) {
